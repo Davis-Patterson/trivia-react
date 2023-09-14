6c9efa9f3c49bdf32088a2ecf458a0d437a36e0d
davis <davisfpatterson@gmail.com>
2023-09-13 20:49:48 -0400

    created 'contexts' directory and moved 'QuesContxt' into it

--- react-trivia/src/components/Quest.jsx
+++ react-trivia/src/components/Quest.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useQuestionContext } from 'components/QuesContxt';
+import { useQuestionContext } from 'contexts/QuesContxt';
 import he from 'he';
 
 const Quest = ({ ques, trivQuesData, showAns, setShowAns, curQuesIdx }) => {
--- react-trivia/src/main.jsx
+++ react-trivia/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { QuestionProvider } from 'components/QuesContxt'; // Import the QuesContxt provider
+import { QuestionProvider } from 'contexts/QuesContxt'; // Import the QuesContxt provider
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
